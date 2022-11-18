f8c7641537817702fb3d817b388aa5265dc02d69
Webdeveloperintraining <103979055+Webdeveloperintraining@users.noreply.github.com>
2022-11-17 16:55:13 -0800

    Update draw_actors_action.py

--- cycle/game/scripting/draw_actors_action.py
+++ cycle/game/scripting/draw_actors_action.py
@@ -26,7 +26,8 @@ class DrawActorsAction(Action):
             cast (Cast): The cast of Actors in the game.
             script (Script): The script of Actions in the game.
         """
-        #score = cast.get_first_actor("scores")
+        score = cast.get_first_actor("scores")
+        score2 = cast.get_first_actor("score2")
         #food = cast.get_first_actor("foods")
         snake = cast.get_first_actor("snakes")
         segments = snake.get_segments()
@@ -38,6 +39,7 @@ class DrawActorsAction(Action):
         #self._video_service.draw_actor(food)
         self._video_service.draw_actors(segments)
         self._video_service.draw_actors(segments2)
-        #self._video_service.draw_actor(score)
+        self._video_service.draw_actor(score)
+        self._video_service.draw_actor(score2)
         self._video_service.draw_actors(messages, True)
         self._video_service.flush_buffer()
